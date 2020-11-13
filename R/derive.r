2bb4ab4bfaa83e994c7004d8fcb835851abb7e12
Yuhao Lin <lyh970817@yandex.com>
2020-11-13 10:34:40 +0000

    Fix `data_derive` and reexport `qualtrics_api_credentials`

--- R/derive.r
+++ R/derive.r
@@ -1,46 +1,3 @@
-#' Upload dictionary to a googlesheet with \code{surveyID} and
-#' \code{survey_name} metadata kept
-#'
-#' Upload dictionary to a googlesheet. \code{surveyID} and
-#' \code{survey_name} are added to the sheet.
-#' @param dict A variable dictionary returned by
-#' \code{\link[qualtdict]{dict_generate}}.
-#' @param url String. An url for a googlesheet page.
-#' @param surveyID Logical. Whether to write \code{surveyID} to the
-#' googlesheet.
-#'
-#' @export
-dict_upload <- function(dict, url, surveyID = FALSE) {
-  dict <- bind_rows(tibble_row(), dict)
-  dict[1, "qid"] <- "surveyName"
-  dict[1, "question"] <- attr(dict, "surveyName")
-
-  if (surveyID) {
-    dict <- bind_rows(tibble_row(), dict)
-    dict[2, "qid"] <- "surveyID"
-    dict[2, "question"] <- attr(dict, "surveyID")
-  }
-  write_sheet(dict, url)
-  invisible(dict)
-}
-
-#' Download dictionary from a googlesheet with \code{surveyID} and
-#' \code{survey_name} metadata
-#'
-#' Download dictionary to a googlesheet. \code{surveyID} and
-#' \code{survey_name} are added to the dictionary as attirbutes.
-#' @param url String. An url for a googlesheet page.
-#'
-#' @export
-dict_download <- function(url) {
-  dict <- read_sheet(url)
-  attr(dict, "surveyName") <- dict[1, "question"]
-  if (dict[2, "qid"] == "surveyID") {
-    attr(dict, "surveyID") <- dict[2, "question"]
-  }
-  dict
-}
-
 #' Add a derived variable to the dictionary
 #'
 #' Add a derived variable to the dictionary. An additional \code{formula}
@@ -73,14 +30,14 @@ add_derived <- function(dict, var, fun) {
 #' \code{\link[qualtdict]{dict_generate}}.
 #'
 #' @export
-data_update <- function(data, dict) {
+data_derive <- function(data, dict) {
   formulae <- dict[["formula"]]
 
   derived_vars <- map2_df(
     formulae[!is.na(formulae)],
     dict[["name"]][!is.na(formulae)],
     function(formula, name) {
-      fun <- eval(str2lang(tl))
+      fun <- eval(str2lang(formula))
       data[name] <- tryCatch(,
         fun(data),
         error = function(cond) {
---
+++ R/upload_download.r
@@ -0,0 +1,42 @@
+#' Upload dictionary to a googlesheet with \code{surveyID} and
+#' \code{survey_name} metadata kept
+#'
+#' Upload dictionary to a googlesheet. \code{surveyID} and
+#' \code{survey_name} are added to the sheet.
+#' @param dict A variable dictionary returned by
+#' \code{\link[qualtdict]{dict_generate}}.
+#' @param url String. An url for a googlesheet page.
+#' @param surveyID Logical. Whether to write \code{surveyID} to the
+#' googlesheet.
+#'
+#' @export
+dict_upload <- function(dict, url, surveyID = FALSE) {
+  dict <- bind_rows(tibble_row(), dict)
+  dict[1, "qid"] <- "surveyName"
+  dict[1, "question"] <- attr(dict, "surveyName")
+
+  if (surveyID) {
+    dict <- bind_rows(tibble_row(), dict)
+    dict[2, "qid"] <- "surveyID"
+    dict[2, "question"] <- attr(dict, "surveyID")
+  }
+  write_sheet(dict, url)
+  invisible(dict)
+}
+
+#' Download dictionary from a googlesheet with \code{surveyID} and
+#' \code{survey_name} metadata
+#'
+#' Download dictionary to a googlesheet. \code{surveyID} and
+#' \code{survey_name} are added to the dictionary as attirbutes.
+#' @param url String. An url for a googlesheet page.
+#'
+#' @export
+dict_download <- function(url) {
+  dict <- read_sheet(url)
+  attr(dict, "surveyName") <- dict[1, "question"]
+  if (dict[2, "qid"] == "surveyID") {
+    attr(dict, "surveyID") <- dict[2, "question"]
+  }
+  dict
+}
