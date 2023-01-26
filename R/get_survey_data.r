6efb85212227b1ec6736bfa32f5094dac9fe71d4
Yuhao Lin <lyh970817@yandex.com>
2023-01-26 21:42:05 +0800

    Format, change comment and shuffle

--- R/get_survey_data.r
+++ R/get_survey_data.r
@@ -46,28 +46,17 @@ get_survey_data <- function(dict,
                             split_by_block = FALSE,
                             skip = NULL,
                             ...) {
-
-  # Validate the dictionary
-  # suppressWarnings(error_list <- dict_validate(dict)$error)
-  # if (!is.null(error_list$non_unique_names)) {
-  #   message("Variables don't have unique names.")
-  #   return(error_list$non_unique_names)
-  # }
-
-  # mistake_qids <- unique(error_list$mistake_dict[["qid"]])
-
   args <- list(...)
   args$force_request <- TRUE
   args$surveyID <- attr(dict, "surveyID")
   args$import_id <- TRUE
   args$convert <- FALSE
   args$label <- FALSE
-  # What about loop and merge?
-  include_qids <- unique(str_extract(dict[["qid"]], "QID[0-9]+"))
 
   survey <- do.call(fetch_survey2, args)
 
-  # Not sure why underscore is appended sometimes when include_questions is specified
+  # Not sure why underscore is appended sometimes
+  # when include_questions is specified
   colnames(survey) <- str_remove(colnames(survey), "_$")
 
   if (!is.null(skip)) {
@@ -97,13 +86,16 @@ get_survey_data <- function(dict,
     return(survey_recode(dict,
       dat = survey, keys = keys,
       unanswer_recode = args$unanswer_recode,
-      unanswer_recode_multi = args$unanswer_recode_multi,
-      numeric_to_pos = numeric_to_pos
+      unanswer_recode_multi = args$unanswer_recode_multi
     ))
   }
 }
 
-survey_recode <- function(dict, dat, keys, unanswer_recode, unanswer_recode_multi, numeric_to_pos) {
+survey_recode <- function(dict,
+                          dat,
+                          keys,
+                          unanswer_recode,
+                          unanswer_recode_multi) {
   in_dat <- dict[["qid"]] %in% colnames(dat)
   dict <- dict[in_dat, ]
   unique_qids <- unique(dict[["qid"]])
@@ -115,7 +107,7 @@ survey_recode <- function(dict, dat, keys, unanswer_recode, unanswer_recode_mult
   dat <- rename(dat[dat_cols], !!!varnames)
 
   # level = unique to preserve ordering
-  split_dict <- split(dict, factor(dict$qid, level = unique(dict$qid)))
+  split_dict <- split(dict, factor(dict$qid, levels = unique(dict$qid)))
   dat_vars <- map2_df(
     dat[unique_varnames], split_dict,
     ~ survey_var_recode(.x, .y,
@@ -127,16 +119,17 @@ survey_recode <- function(dict, dat, keys, unanswer_recode, unanswer_recode_mult
   bind_cols(dat[keys], dat_vars)
 }
 
-survey_var_recode <- function(var, var_dict, unanswer_recode, unanswer_recode_multi, numeric_to_pos) {
+survey_var_recode <- function(var,
+                              var_dict,
+                              unanswer_recode,
+                              unanswer_recode_multi) {
   # Multiple rows for a question, only first one chosen
   type <- var_dict[["type"]][1]
-  selector <- var_dict[["selector"]][1]
   content_type <- var_dict[["content_type"]][[1]]
   levels <- var_dict[["level"]]
   labels <- var_dict[["label"]]
 
   if (!is.na(content_type) && content_type == "Number") {
-
     # Check for content_type numeric,
     # vector with numbers such as "06" passes validation on Qualtrics but
     # will be character when read by readr
@@ -165,9 +158,11 @@ survey_var_recode <- function(var, var_dict, unanswer_recode, unanswer_recode_mu
   # TE variables dont have levels or labels
   if (any(!is.na(levels))) {
     var <- set_labels(var, labels = setNames(levels, labels))
+  } else {
+    text_label <- unique(paste_narm(var_dict[["question"]], var_dict[["item"]]))
+    var <- set_label(var, label = text_label)
   }
-  text_label <- unique(paste_narm(var_dict[["question"]], var_dict[["item"]]))
-  var <- set_label(var, label = text_label)
+
 
   return(var)
 }
