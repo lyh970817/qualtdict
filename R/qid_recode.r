e59db2dc0b2c8b2b150d029972c521f0227136d3
Yuhao Lin <lyh970817@yandex.com>
2023-01-27 13:17:00 +0800

    Change recode_json.r
    1. Some shuffling and formatting
    2. Change 'choice_len' to 'level_len' and apply to SBS
    3. Add "TEXT" suffix to "TE" type question in SBS
    4. Move 'qid_recode' to a different file

---
+++ R/qid_recode.r
@@ -0,0 +1,480 @@
+questiontext_qid <- function(qid,
+                             col_len,
+                             item,
+                             level,
+                             label,
+                             choice_len,
+                             col_type) {
+  qid
+}
+
+add_text_mc <- function(new_qid, level) {
+  text_pos <- grep("TEXT", level)
+  if (!is.null(text_pos)) {
+    new_qid[text_pos] <- paste(new_qid, names(level), sep = "_")[text_pos]
+  }
+  new_qid
+}
+
+suf_level_qid <- function(qid,
+                          col_len,
+                          item,
+                          level,
+                          label,
+                          choice_len,
+                          col_type) {
+  add_text_mc(paste(qid, level, sep = "_"), level)
+}
+
+suf_nmlabel_qid <- function(qid,
+                            col_len,
+                            item,
+                            level,
+                            label,
+                            choice_len,
+                            col_type) {
+  # Same as level?
+  paste(qid, names(label), sep = "_")
+}
+
+suf_text_qid <- function(qid,
+                         col_len,
+                         item,
+                         level,
+                         label,
+                         choice_len,
+                         col_type) {
+  paste(qid, "TEXT", sep = "_")
+}
+
+rep_level_qid <- function(qid,
+                          col_len,
+                          item,
+                          level,
+                          label,
+                          choice_len,
+                          col_type) {
+  add_text_mc(rep(qid, length(level)), level)
+}
+
+suf_item_suf_level_qid <- function(qid,
+                                   col_len,
+                                   item,
+                                   level,
+                                   label,
+                                   choice_len,
+                                   col_type) {
+  paste_narm(qid, names(item), sep = "_") %>%
+    map(paste, level, sep = "_") %>%
+    unlist()
+}
+
+suf_level_suf_item_qid <- function(qid,
+                                   col_len,
+                                   item,
+                                   level,
+                                   label,
+                                   choice_len,
+                                   col_type) {
+  paste_narm(qid, level, sep = "_") %>%
+    map(paste, names(item), sep = "_") %>%
+    unlist()
+}
+
+suf_item_rep_level_qid <- function(qid,
+                                   col_len,
+                                   item,
+                                   level,
+                                   label,
+                                   choice_len,
+                                   col_type) {
+  paste_narm(qid, names(item), sep = "_") %>%
+    rep_qid(item, choice_len)
+}
+
+item_or_level_qid <- function(qid,
+                              col_len,
+                              item,
+                              level,
+                              label,
+                              choice_len,
+                              col_type) {
+  if (is.null(item)) {
+    suf_level_qid(
+      qid,
+      col_len,
+      item,
+      level,
+      label,
+      choice_len,
+      col_type
+    )
+  } else {
+    suf_item_rep_level_qid(
+      qid,
+      col_len,
+      item,
+      level,
+      label,
+      choice_len,
+      col_type
+    )
+  }
+}
+
+text <- function(qid,
+                 col_len,
+                 item,
+                 level,
+                 label,
+                 choice_len) {
+  paste(qid, "TEXT", sep = "_")
+}
+
+sbs_qid <- function(qid,
+                    col_len,
+                    item,
+                    level,
+                    label,
+                    choice_len,
+                    col_type) {
+  names(choice_len) <- col_type
+  paste(qid, sep = "#", seq(col_len)) %>%
+    map2(length(item), rep) %>%
+    map(paste, names(item), sep = "_") %>%
+    map2(choice_len, ~ rep(.x, each = .y)) %>%
+    map2(col_type, function(qids, type) {
+      if (type == "TE") {
+        return(paste(qids, 1:choice_len[type], sep = "_"))
+      } else {
+        return(qids)
+      }
+    }) %>%
+    unlist()
+}
+
+timing_qid <- function(qid,
+                       col_len,
+                       item,
+                       level,
+                       label,
+                       choice_len,
+                       col_type) {
+  paste0(qid, c(
+    "_FIRST_CLICK", "_LAST_CLICK", "_PAGE_SUBMIT",
+    "_CLICK_COUNT"
+  ))
+}
+
+file_upload_qid <- function(qid,
+                            col_len,
+                            item,
+                            level,
+                            label,
+                            choice_len,
+                            col_type) {
+  paste0(qid, c(
+    "_FILE_ID", "_FILE_NAME", "_FILE_SIZE", "_FILE_TYPE"
+  ))
+}
+
+not_applicable_qid <- function(qid,
+                               col_len,
+                               item,
+                               level,
+                               label,
+                               choice_len,
+                               col_type) {
+  warn_msg <- paste0(
+    qid, " is an unsupported type of question."
+  )
+  warning(warn_msg)
+  qid
+}
+
+qid_recode <- function(qid,
+                       col_len,
+                       item,
+                       level,
+                       label,
+                       choice_len,
+                       col_type,
+                       type,
+                       selector,
+                       sub_selector,
+                       is_qid) {
+  # print(c(qid, type, selector, sub_selector))
+
+  add_text_mc <- function(new_qid, level) {
+    text_pos <- grep("TEXT", level)
+    if (!is.null(text_pos)) {
+      new_qid[text_pos] <- paste(new_qid, names(level), sep = "_")[text_pos]
+    }
+    new_qid
+  }
+
+  suf_level_qid <- function(qid,
+                            col_len,
+                            item,
+                            level,
+                            label,
+                            choice_len,
+                            col_type) {
+    add_text_mc(paste(qid, level, sep = "_"), level)
+  }
+
+  suf_nmlabel_qid <- function(qid,
+                              col_len,
+                              item,
+                              level,
+                              label,
+                              choice_len,
+                              col_type) {
+    # Same as level?
+    paste(qid, names(label), sep = "_")
+  }
+
+  suf_text_qid <- function(qid,
+                           col_len,
+                           item,
+                           level,
+                           label,
+                           choice_len,
+                           col_type) {
+    paste(qid, "TEXT", sep = "_")
+  }
+
+  rep_level_qid <- function(qid,
+                            col_len,
+                            item,
+                            level,
+                            label,
+                            choice_len,
+                            col_type) {
+    add_text_mc(rep(qid, length(level)), level)
+  }
+
+  suf_item_suf_level_qid <- function(qid,
+                                     col_len,
+                                     item,
+                                     level,
+                                     label,
+                                     choice_len,
+                                     col_type) {
+    paste_narm(qid, names(item), sep = "_") %>%
+      map(paste, level, sep = "_") %>%
+      unlist()
+  }
+
+  suf_level_suf_item_qid <- function(qid,
+                                     col_len,
+                                     item,
+                                     level,
+                                     label,
+                                     choice_len,
+                                     col_type) {
+    paste_narm(qid, level, sep = "_") %>%
+      map(paste, names(item), sep = "_") %>%
+      unlist()
+  }
+
+  suf_item_rep_level_qid <- function(qid,
+                                     col_len,
+                                     item,
+                                     level,
+                                     label,
+                                     choice_len,
+                                     col_type) {
+    paste_narm(qid, names(item), sep = "_") %>%
+      rep_qid(item, choice_len)
+  }
+
+  item_or_level_qid <- function(qid,
+                                col_len,
+                                item,
+                                level,
+                                label,
+                                choice_len,
+                                col_type) {
+    if (is.null(item)) {
+      suf_level_qid(
+        qid,
+        col_len,
+        item,
+        level,
+        label,
+        choice_len,
+        col_type
+      )
+    } else {
+      suf_item_rep_level_qid(
+        qid,
+        col_len,
+        item,
+        level,
+        label,
+        choice_len,
+        col_type
+      )
+    }
+  }
+
+  text <- function(qid,
+                   col_len,
+                   item,
+                   level,
+                   label,
+                   choice_len) {
+    paste(qid, "TEXT", sep = "_")
+  }
+
+  sbs_qid <- function(qid,
+                      col_len,
+                      item,
+                      level,
+                      label,
+                      choice_len,
+                      col_type) {
+    names(choice_len) <- col_type
+    paste(qid, sep = "#", seq(col_len)) %>%
+      map2(length(item), rep) %>%
+      map(paste, names(item), sep = "_") %>%
+      map2(choice_len, ~ rep(.x, each = .y)) %>%
+      map2(col_type, function(qids, type) {
+        if (type == "TE") {
+          return(paste(qids, 1:choice_len[type], sep = "_"))
+        } else {
+          return(qids)
+        }
+      }) %>%
+      unlist()
+  }
+
+  timing_qid <- function(qid,
+                         col_len,
+                         item,
+                         level,
+                         label,
+                         choice_len,
+                         col_type) {
+    paste0(qid, c(
+      "_FIRST_CLICK", "_LAST_CLICK", "_PAGE_SUBMIT",
+      "_CLICK_COUNT"
+    ))
+  }
+
+  file_upload_qid <- function(qid,
+                              col_len,
+                              item,
+                              level,
+                              label,
+                              choice_len,
+                              col_type) {
+    paste0(qid, c(
+      "_FILE_ID", "_FILE_NAME", "_FILE_SIZE", "_FILE_TYPE"
+    ))
+  }
+
+  not_applicable_qid <- function(qid,
+                                 col_len,
+                                 item,
+                                 level,
+                                 label,
+                                 choice_len,
+                                 col_type) {
+    warn_msg <- paste0(
+      qid, " is an unsupported type of question."
+    )
+    warning(warn_msg)
+    qid
+  }
+
+  recode_list <- list(
+    MC =
+      list(
+        MACOL = list(TX = suf_level_qid),
+        MAVR = list(TX = suf_level_qid),
+        MAHR = list(TX = suf_level_qid),
+        MSB = suf_level_qid,
+        SAVR = list(TX = rep_level_qid),
+        SACOL = list(TX = rep_level_qid),
+        DL = rep_level_qid,
+        SAHR = list(TX = rep_level_qid),
+        SB = rep_level_qid,
+        NPS = rep_level_qid
+      ),
+    Matrix =
+      list(
+        Likert = list(
+          MultipleAnswer = suf_item_suf_level_qid,
+          DL = suf_item_rep_level_qid,
+          SingleAnswer = item_or_level_qid,
+          DND = item_or_level_qid
+        ),
+        TE = list(Short = suf_item_suf_level_qid),
+        Profile = suf_item_rep_level_qid,
+        Bipolar = suf_item_rep_level_qid,
+        RO = suf_item_suf_level_qid,
+        MaxDiff = suf_item_rep_level_qid,
+        CS = list(WTB = suf_item_suf_level_qid)
+      ),
+    Slider = list(
+      HSLIDER = suf_level_qid,
+      HBAR = suf_level_qid,
+      STAR = suf_level_qid
+    ),
+    CS = list(
+      HR = list(TX = item_or_level_qid),
+      VRTL = list(TX = item_or_level_qid),
+      HBAR = item_or_level_qid,
+      HSLIDER = item_or_level_qid
+    ),
+    TE = list(
+      FORM = suf_nmlabel_qid,
+      SL = suf_text_qid,
+      ML = suf_text_qid,
+      ESTB = suf_text_qid
+    ),
+    SBS = list(SBSMatrix = sbs_qid),
+    Timing = list(PageTimer = timing_qid),
+    SS = list(TA = rep_level_qid),
+    FileUpload = list(FileUpload = file_upload_qid),
+    PGR = list(DragAndDrop = list(NoColumns = not_applicable_qid)),
+    DD = list(DL = suf_item_rep_level_qid),
+    Draw = list(Signature = file_upload_qid),
+    HL = list(Text = suf_level_suf_item_qid),
+    Meta = list(Browser = not_applicable_qid),
+    DB = list(TB = questiontext_qid)
+  )
+
+  if (type != "SBS") {
+    level <- level[[1]]
+    label <- label[[1]]
+  }
+
+  if (!is.null(selector)) {
+    if (!is.null(sub_selector)) {
+      recode_func <- recode_list[[type]][[selector]][[sub_selector]]
+    } else {
+      recode_func <- recode_list[[type]][[selector]]
+    }
+  } else {
+    recode_func <- recode_list[[type]][["MC"]]
+  }
+
+  if (is.null(recode_func)) {
+    recode_func <- not_applicable_qid
+  }
+
+  new_qid <- recode_func(
+    qid,
+    col_len,
+    item,
+    level,
+    label,
+    choice_len,
+    col_type
+  )
+
+  return(new_qid)
+}
--- R/recode_json.r
+++ R/recode_json.r
@@ -96,9 +96,6 @@ recode_json <- function(surveyID,
     })
 
   json <- imap(question_meta, function(qjson, qid) {
-    # If no subquestion or choice, treat the number as 0
-    sub_q_len <- length(qjson$subQuestions) %>% ifelse(. > 0, ., 1)
-
     # Clean the &nbsp; level/label fields (empty on Qualtrics)
     nbsps <- map(qjson$choices, "description") == "&nbsp;"
 
@@ -107,8 +104,6 @@ recode_json <- function(surveyID,
       qjson$choices <- qjson$choices[!nbsps]
     }
 
-    choice_len <- length(qjson$choices) %>% ifelse(. > 0, ., 1)
-
     question_name <- qjson$questionName
     type <- qjson$questionType$type
     question <- qjson$questionText
@@ -116,17 +111,21 @@ recode_json <- function(surveyID,
     block <- qjson$block
     content_type <- qjson$content_type
 
+    # If no subquestion or choice, treat subquestion length as 1
+    sub_q_len <- length(qjson$subQuestions) %>% ifelse(. > 0, ., 1)
+
+    # If no levels, treat level length as 1
+    level_len <- length(qjson$choices) %>% ifelse(. > 0, ., 1)
+
+     unlist_n <- function(list) {
+       # unlist that preserves names
+       names <- names(list)
+       v <- unlist(map(list, null_na)) %>%
+         setNames(names)
+       return(v)
+     }
     # The rep_level function works on lists for dealing with SBS questions
     # For consistency we convert to lists for non-SBS questions
-
-    unlist_n <- function(list) {
-      # unlist that preserves names
-      names <- names(list)
-      v <- unlist(map(list, null_na)) %>%
-        setNames(names)
-      return(v)
-    }
-
     level <- map(qjson$choices, "recode") %>%
       unlist_n() %>%
       list()
@@ -135,8 +134,7 @@ recode_json <- function(surveyID,
       unlist_n() %>%
       list()
 
-
-    # Recode QIDs for  text entry choices
+    # Recode for text entry choices
     has_text <- which(map_lgl(qjson$choices, ~ "textEntry" %in% names(.x)))
 
     if (length(has_text) > 0) {
@@ -145,53 +143,61 @@ recode_json <- function(surveyID,
       label <- add_text(label, has_text)
     }
 
-    # Recode QIDs for  text entry item
-    has_text_sub <- which(map_lgl(qjson$subQuestions, ~ "textEntry" %in% names(.x)))
     item <- unlist(map(qjson$subQuestions, "choiceText"))
     sub_selector <- qjson$questionType$subSelector
+
+    # Recode for text entry item
+    has_text_sub <- which(map_lgl(
+      qjson$subQuestions, ~ "textEntry" %in% names(.x)
+    ))
     if (length(has_text_sub) > 0) {
       item <- unlist(add_text(item, has_text_sub))
       sub_q_len <- sub_q_len + length(has_text_sub)
     }
 
-    # Get number of levels in each column
-    level_len_col <- map(qjson$columns, "choices") %>% map_dbl(length)
-    # Calculate column length
-    col_len <- length(qjson$columns)
-    # Get column types
-    col_type <- map_chr(qjson$columns, ~ .x$questionType$selector)
+    if (type == "SBS") {
+      # Get number of levels in each column
+      level_len <- map(qjson$columns, "choices") %>% map_dbl(length)
+      # Calculate column length
+      col_len <- length(qjson$columns)
+      # Get column types
+      col_type <- map_chr(qjson$columns, ~ .x$questionType$selector)
+      if (col_len != 0) {
+        # Zero length columns means it's a carried forward question
 
-    # Zero length columns means it's a carried forward question
-    if (type == "SBS" & col_len != 0) {
-      level_lens <- map(qjson$columns, "choices") %>% map_dbl(length)
-      choice_len <- level_len_col
+        # Get overacching question
+        top_question <- qjson$questionText
+        # Get questions in each column
+        question <- map(qjson$columns, "questionText") %>%
+          # Repeat the question for the number of levels, separately for each
+          # column
+          map2(level_len, ~ rep(.x, each = .y)) %>%
+          unlist() %>%
+          # Repeat for the number of items
+          rep(each = sub_q_len) %>%
+          # Prepend the overarching question
+          paste(top_question, ., sep = " ")
 
-      # Get overacching question
-      top_question <- qjson$questionText
-      # Get questions in each column
-      question <- map(qjson$columns, "questionText") %>%
-        # Repeat the question for the number of levels, separately for each
-        # column
-        map2(level_len_col, ~ rep(.x, each = .y)) %>%
-        unlist() %>%
-        # Repeat for the number of items
-        rep(each = sub_q_len) %>%
-        # Prepend the overarching question
-        paste(top_question, ., sep = " ")
+        level <- map(qjson$columns, "choices") %>%
+          map(~ map_chr(.x, "recode")) %>%
+          map2(col_type, function(level, type) {
+            if (type == "TE") {
+              level <- paste(level, "TEXT", sep = "_")
+            }
+            level
+          })
 
-      level <- map(qjson$columns, "choices") %>%
-        map(~ map_chr(.x, "recode"))
+        label <- map(qjson$columns, "choices") %>%
+          map(~ map_chr(.x, "description"))
 
-      label <- map(qjson$columns, "choices") %>%
-        map(~ map_chr(.x, "description"))
-
-      item <- unlist(map(qjson$subQuestions, "description"))
+        item <- unlist(map(qjson$subQuestions, "description"))
+      }
     }
 
     new_qid <- qid_recode(qid,
       col_len = col_len, col_type = col_type, 
       item = item, level = level, label = label,
-      choice_len = choice_len, level_len_col = level_len_col,
+      choice_len = level_len,
       type = type, selector = selector, 
       sub_selector = sub_selector, is_qid = TRUE
     )
@@ -199,7 +205,7 @@ recode_json <- function(surveyID,
     question_name <- qid_recode(question_name,
       col_len = col_len, col_type = col_type, 
       item = item, level = level, label = label,
-      choice_len = choice_len, level_len_col = level_len_col,
+      choice_len = level_len,
       type = type, selector = selector, 
       sub_selector = sub_selector, is_qid = FALSE
     )
@@ -210,7 +216,7 @@ recode_json <- function(surveyID,
       block = block,
       question = question,
       looping_question = NA,
-      item = rep_item(item, choice_len) %>% null_na(),
+      item = rep_item(item, level_len) %>% null_na(),
       level = rep_level(level, item) %>% null_na(),
       label = rep_level(label, item) %>% null_na(),
       type = type, selector = selector, content_type = content_type,
@@ -349,121 +355,4 @@ to_dataframe <- function(json) {
   map_df(json, function(qmeta) {
     map_df(qmeta, unlist) 
   })
-}
-
-qid_recode <- function(qid,
-                       col_len,
-                       item,
-                       level,
-                       label = label,
-                       choice_len,
-                       level_len_col,
-                       col_type,
-                       type,
-                       selector,
-                       sub_selector,
-                       is_qid) {
-  if (type != "SBS") {
-    level <- level[[1]]
-    label <- label[[1]]
-  }
-
-  if (type == "MC") {
-    if (selector == "MACOL" || selector == "MAVR" || selector == "MAHR") {
-      new_qid <- paste(qid, level, sep = "_")
-    } else if (selector == "SAVR" || selector == "SACOL" || selector == "DL" || selector == "SAHR") {
-      new_qid <- rep(qid, length(level))
-    }
-
-    text_pos <- grep("TEXT", level)
-    if (!is.null(text_pos)) {
-      new_qid[text_pos] <- paste(qid, names(level), sep = "_")[text_pos]
-    }
-  } else if (type == "Matrix") {
-    if (selector == "Likert") {
-      if (sub_selector == "MultipleAnswer") {
-        new_qid <- paste_narm(qid, names(item), sep = "_") %>%
-          map(paste, level, sep = "_") %>%
-          unlist()
-      } else if (sub_selector == "DL") {
-        new_qid <- paste_narm(qid, names(item), sep = "_") %>%
-          rep_qid(item, choice_len)
-      } else if (sub_selector == "SingleAnswer") {
-        if (is.null(item)) {
-          new_qid <- paste(qid, names(level), sep = "_")
-        } else {
-          new_qid <- paste(qid, names(item), sep = "_") %>%
-            rep_qid(item, choice_len)
-        }
-      }
-    } else if (selector == "TE") {
-      new_qid <- paste(qid, names(item), sep = "_") %>%
-        rep(each = choice_len) %>%
-        paste(level, sep = "_")
-    } else if (selector == "Profile" || selector == "Bipolar") {
-      new_qid <- paste(qid, names(item), sep = "_") %>%
-        rep_qid(item, choice_len)
-    }
-  } else if (type == "Slider" && selector == "HSLIDER") {
-    if (is.null(item)) {
-      new_qid <- paste(qid, names(level), sep = "_")
-    } else {
-      new_qid <- paste(qid, names(item), sep = "_") %>%
-        rep_item(choice_len)
-    }
-  } else if (type == "Slider" && selector == "HBAR") {
-    new_qid <- paste(qid, level, sep = "_")
-  } else if (type == "Slider" && selector == "STAR") {
-    new_qid <- paste(qid, level, sep = "_")
-  } else if (type == "TE" && selector == "FORM") {
-    new_qid <- paste(qid, names(label), sep = "_")
-  } else if (type == "TE" &&
-    (selector == "SL" ||
-      selector == "ML" ||
-      selector == "ESTB")) {
-    new_qid <- paste(qid, "TEXT", sep = "_")
-  } else if (type == "SBS") {
-    new_qid <- paste(qid, sep = "#", seq(col_len)) %>%
-      map2(length(item), rep) %>%
-      map(paste, names(item), sep = "_") %>%
-      map2(level_len_col, ~ rep(.x, each = .y)) %>%
-      map2(col_type, function(qids, type) {
-        if (type == "TE") {
-          return(paste(qids, "1", sep = "_"))
-        } else {
-          return(qids)
-        }
-      }) %>%
-      unlist()
-  } else if (type == "CS") {
-    if (selector == "HR") {
-      if (sub_selector == "TX") {
-        # Is it possible to have item?
-        if (is.null(item)) {
-          new_qid <- paste(qid, names(level), sep = "_")
-        } else {
-          new_qid <- paste(qid, names(item), sep = "_") %>%
-            rep_item(choice_len)
-        }
-      }
-    }
-  } else {
-    warn_msg <- glue::glue(
-      "{qid} is an unsupported type of question with type = {type}, selector = {selector} and sub_selector = {sub_selector}.
-    Please report this to the issue page of the qualtdict package."
-    )
-
-    warning(warn_msg, call. = FALSE)
-    new_qid <- qid
-  }
-  # Some questions might have no choices which produces NULL
-  if (is.null(new_qid)) {
-    return(qid)
-  }
-
-  # This needs to be refactored so that with supported 'type' but
-  # unsupported 'selector' or 'sub_selector' there is still a warning
-  # msg
-
-  return(new_qid)
 }
