a443b559074225ede96e0c4b699180da84d68a35
lyh970817 <lyh970817@yandex.com>
2026-07-01 03:44:46 +0800

    Add normalised metadata constructor

--- tests/testthat/test-metadata_normalise.R
+++ tests/testthat/test-metadata_normalise.R
@@ -22,3 +22,36 @@ test_that("metadata normalisation preserves representative dictionary rows", {
 
   expect_snapshot_value(metadata_dictionary_summary(dict), style = "json2")
 })
+
+test_that("normalised metadata constructor preserves current object shape", {
+  raw_metadata <- synthetic_mc_text_raw_metadata()
+  survey_question_facts <- normalise_qualtrics_questions(
+    raw_metadata$metadata,
+    raw_metadata$description
+  )
+
+  normalised_metadata <- new_normalised_metadata(
+    survey_id = "SV_TEST",
+    survey_name = "Constructor Survey",
+    survey_question_facts = survey_question_facts,
+    embedded_data_fields = empty_normalised_embedded_data_fields(),
+    scoring_variables = empty_normalised_scoring_variables(),
+    text_analysis_sidecars = empty_normalised_text_analysis_sidecars()
+  )
+
+  expect_s3_class(normalised_metadata, "qualtdict_normalised_metadata")
+  expect_named(
+    normalised_metadata,
+    c(
+      "surveyID",
+      "survey_name",
+      "questions",
+      "embedded_data",
+      "scoring",
+      "text_analysis"
+    )
+  )
+  expect_identical(normalised_metadata$surveyID, "SV_TEST")
+  expect_identical(normalised_metadata$survey_name, "Constructor Survey")
+  expect_identical(normalised_metadata$questions, survey_question_facts)
+})
